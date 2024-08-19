f04608c70603e0b08439e86ea1d4c0b8d4736283
Satu Kulta <satu.kulta@myy.haaga-helia.fi>
2024-08-19 20:51:01 +0300

    Laskin toteutettu

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [numbers, setNumbers] = useState({
+    first: '',
+    second: '',
+  });
+  const [result, setResult] = useState('');
+
+  const handleTotal = () => {
+    setResult(Number(numbers.first) + Number(numbers.second));
+  }
+
+  const handleDifference = () => {
+    setResult(parseFloat(numbers.first) - parseFloat(numbers.second));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.result}>Result: {result}</Text>
+      <TextInput
+        style={styles.input}
+        value={numbers.first}
+        inputMode='numeric'
+        returnKeyType='done'
+        clearTextOnFocus={true}
+        onFocus={() => setResult('')}
+        onChangeText={text => setNumbers({ ...numbers, first: text })}
+      />
+      <TextInput
+        style={styles.input}
+        value={numbers.second}
+        inputMode='numeric'
+        returnKeyType='done'
+        clearTextOnFocus={true}
+        onFocus={() => setResult('')}
+        onChangeText={text => setNumbers({ ...numbers, second: text })}
+      />
+      <View style={styles.buttonContainer}>
+        <Button size="lg" title='+' onPress={handleTotal} />
+        <Button title='-' onPress={handleDifference}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +50,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff8f9',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  result: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: 'gray',
+    width: 150,
+    padding: 8
+  },
+  buttonContainer: {
+    flexDirection: 'row',
   },
 });
